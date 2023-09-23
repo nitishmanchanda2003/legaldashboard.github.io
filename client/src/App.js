12e4edf8975b7e20a9d784ec8c04208cffb934ed
nitishmanchanda2003 <104716981+nitishmanchanda2003@users.noreply.github.com>
2023-09-23 15:49:46 +0530

    Lawyer Dashboard

---
+++ client/src/App.js
@@ -0,0 +1,50 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
+import Layout from "scenes/layout";
+import Dashboard from "scenes/dashboard";
+import Cases from "scenes/cases";
+import Clients from "scenes/clients";
+import Payments from "scenes/payments";
+import Geography from "scenes/geography";
+import Calendar from "scenes/calendar/Calendar";
+import BoardPage from "scenes/todolist/Board";
+// import Monthly from "scenes/monthly";
+// import Breakdown from "scenes/breakdown";
+// import Admin from "scenes/admin";
+// import Performance from "scenes/performance";
+
+function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/cases" element={<Cases />} />
+              <Route path="/clients" element={<Clients />} />
+              <Route path="/payments" element={<Payments />} />
+              <Route path="/geography" element={<Geography />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/todolist" element={<BoardPage />} />
+              {/*<Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<Breakdown />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/performance" element={<Performance />} /> */}
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/state/Board.js
@@ -0,0 +1,8 @@
+import {create} from 'zustand'
+import { boardData } from 'data'
+const useBoard = create((set)=> ({
+    board: boardData,
+    setBoard: (board)=> set((state)=> ({board}))
+}))
+
+export default useBoard
